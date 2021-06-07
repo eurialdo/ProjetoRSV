41036e81a44aa3e00affd582e664f27aab01e392
eurialdo <eurialdomendes@gmail.com>
2021-06-07 19:23:25 -0300

    Eurialdo
    versão 1.1

--- RSV.py
+++ RSV.py
@@ -5,11 +5,14 @@ import os
 
 en = pyttsx3.init()
 
+def voz ():
+    en.setProperty('voice', b'brazil')
+    en.setProperty('rate', 180)
+    en.setProperty('volume', 1)
+    en.runAndWait()
+
 en.say('olá, o que você deseja fazer nesse momento')
-en.setProperty('voice', b'brazil')
-en.setProperty('rate', 180)
-en.setProperty('volume', 1)
-en.runAndWait()
+voz()
 
 recon =sr.Recognizer()
 with sr.Microphone() as source:
@@ -17,51 +20,55 @@ with sr.Microphone() as source:
         audio = recon.listen(source)
         resposta = recon.recognize_google(audio,language='pt')
         print(resposta)
-        if resposta == "desejo acessar o Google":
+        if resposta == "Abrir Google":
             en.say('Ok, abrindo seu navegador na página do google')
-            en.setProperty('voice', b'brazil')
-            en.setProperty('rate', 180)
-            en.setProperty('volume', 1)
-            en.runAndWait()
+            voz()
 
             webbrowser.open("www.google.com")
 
-        elif resposta == "desejo acessar o YouTube":
+        elif resposta == "abrir YouTube":
             en.say('ok, abrindo o youtube')
-            en.setProperty('voice', b'brazil')
-            en.setProperty('rate', 180)
-            en.setProperty('volume', 1)
-            en.runAndWait()
+            voz()
             webbrowser.open("www.youtube.com.br")
 
-        elif resposta == "desejo acessar notícia":
+        elif resposta =="quero fazer um cálculo":
+            while True:
+                en.say('informe os números que deseja multiplicar')
+                voz()
+                audio = recon.listen(source)
+                soma = recon.recognize_google(audio, language='pt')
+                if soma == 'obrigado pela ajuda': break
+                print("sua multiplicação: " + str(soma))
+                Resultado = str(int(soma[0]) * int(soma[4]))
+                print("o resultado é"+ Resultado)
+                en.say('O resultado é'+ str(Resultado))
+                voz()
+
+        elif resposta == "acessar notícia":
             en.say('OK, acessando as noticias...')
-            en.setProperty('voice', b'brazil')
-            en.setProperty('rate', 180)
-            en.setProperty('volume', 1)
-            en.runAndWait()
+            voz()
             webbrowser.open('www.terra.com.br')
 
-        elif resposta=="desejo ouvir música":
+        elif resposta=="ouvir música":
             en.say('Ok, executando a música')
-            en.setProperty('voice',b'brazil')
-            en.setProperty('rate', 180)
-            en.setProperty('volume', 1)
-            en.runAndWait()
+            voz()
             os.system('musica.mp3')
 
-        elif resposta == "obrigado":
+        elif resposta == "quero assistir Prime vídeo":
+            en.say('OK, vamos assistir Prime vídeo...')
+            voz
+            webbrowser.open('www.primevideo.com')
+
+        elif resposta == "Quero ver minhas notas de sistemas multimídias":
+            voz()
+            webbrowser.open('https://docs.google.com/spreadsheets/d/1-gDh2ois13yJ8lrLl_4-2BkQFd-qMo-XKcHGbvDUYQs/edit#gid=0')
+
+        elif resposta == "Obrigado pela ajuda":
             en.say('OK, até mais...')
-            en.setProperty('voice', b'brazil')
-            en.setProperty('rate', 180)
-            en.setProperty('volume', 1)
-            en.runAndWait()
+            voz()
             break
 
         else:
             en.say('Desculpe comando não reconhecido, por favor repita')
-            en.setProperty('voz', b'brazil')
-            en.setProperty('rate', 180)
-            en.setProperty('volume', 1)
-            en.runAndWait()
+            voz()
             print('Desculpe comando não reconhecido...')
